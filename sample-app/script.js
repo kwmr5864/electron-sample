a613a369053e5b32b9f864bf38955f0a899c30d6
Masayuki Kawamura <kwmr5864@gmail.com>
2015-09-02 06:20:43 +0900

    入力フォームをテンプレート化して分離

--- sample-app/script.js
+++ sample-app/script.js
@@ -1,5 +1,6 @@
 angular.module('app', []).controller('appCtrl', function($scope) {
     $scope.message = '';
+    $scope.formUrl = 'partials/form.html';
     $scope.hello = function() {
         alert(this.message);
     };
