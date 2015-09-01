7f5a8ecf793f5368655d14077d86450468b3e1a3
Masayuki Kawamura <kwmr5864@gmail.com>
2015-09-02 08:46:46 +0900

    番号と登録日を追加

--- sample-app/script.js
+++ sample-app/script.js
@@ -2,7 +2,13 @@ angular.module('app', []).controller('appCtrl', function($scope) {
     $scope.message = '';
     $scope.formUrl = 'partials/form.html';
     $scope.messages = [];
+    $scope.index = 0;
     $scope.add = function() {
-        $scope.messages.push(this.message);
+        $scope.messages.push({
+            id: $scope.index,
+            value: this.message,
+            created_at: +new Date()
+        });
+        $scope.index++;
     };
 });
