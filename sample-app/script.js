eed945ac7066236df8db23fcaafbc34c09fe5e18
Masayuki Kawamura <kwmr5864@gmail.com>
2015-09-02 22:05:25 +0900

    データをLocalStorageに永続的に保存

--- sample-app/script.js
+++ sample-app/script.js
@@ -1,14 +1,17 @@
 angular.module('app', []).controller('appCtrl', function($scope) {
+    initStorageMessages();
     $scope.message = '';
     $scope.formUrl = 'partials/form.html';
-    $scope.messages = [];
-    $scope.index = 0;
+    $scope.messages = getStorageMessages();
+    $scope.index = $scope.messages.length < 1 ? 0 : $scope.messages[$scope.messages.length - 1].id;
     $scope.add = function() {
-        $scope.messages.push({
+        var message = {
             id: $scope.index,
             value: this.message,
             created_at: +new Date()
-        });
+        };
+        $scope.messages.push(message);
+        saveStorageMessages($scope.messages);
         $scope.index++;
     };
     $scope.del = function(id) {
@@ -18,5 +21,27 @@ angular.module('app', []).controller('appCtrl', function($scope) {
                 break;
             }
         }
+        saveStorageMessages($scope.messages);
     };
 });
+
+function initStorageMessages() {
+    if (!localStorage.messages) {
+        localStorage.messages = [];
+    }
+}
+
+function getStorageMessages() {
+    var messages = [];
+    if (localStorage.messages) {
+        storageMessages = JSON.parse(localStorage.messages);
+        for (var i in storageMessages) {
+            messages.push(storageMessages[i]);
+        }
+    }
+    return messages;
+}
+
+function saveStorageMessages(messages) {
+    localStorage.messages = JSON.stringify(messages);
+}
